82be54385bfff8991962182074a9a07276d9510a
fatima zahrae <fatimazahraeelghiouan69@gmail.com>
2025-05-16 00:13:14 -0700

    ADD files

---
+++ src/app/catlog/catlog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catlog',
+  imports: [],
+  templateUrl: './catlog.component.html',
+  styleUrl: './catlog.component.css'
+})
+export class CatlogComponent {
+
+}
